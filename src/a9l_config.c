b8353e95b42c5c0fa3596d30924a00c3945fe2e6
Gabriel Marcano <gabemarcano@yahoo.com>
2016-08-07 01:15:59 -0400

    Configuration file parsing improvements
    -Config entries no longer need to be in any specific order. Offset has
    been made optional.

--- src/a9l_config.c
+++ src/a9l_config.c
@@ -58,6 +58,95 @@ void a9l_config_entry_destroy(a9l_config_entry *entry)
 	entry->buttons = 0;
 }
 
+static bool validate_json_entry_token(const char* json, const jsmntok_t *tokens, size_t amount, size_t *current_element)
+{
+	size_t i = *current_element;
+
+	//Mandatory entries: name, location, buttons(for now)
+	bool name_found = false, location_found = false, buttons_found = false;
+	bool offset_found = false;
+
+	//Should be the object wrapping an entry
+	if (++i >= amount || tokens[i].type != JSMN_OBJECT)
+		return false;
+
+	size_t number_of_entries = (size_t)tokens[i].size;
+	if (number_of_entries < 3)
+		return false;
+
+	for (size_t entry = 0; entry < number_of_entries; ++entry)
+	{
+		//Should be "name", "location", "offset". or "buttons"
+		if (++i >= amount ||
+			tokens[i].type != JSMN_STRING)
+			return false;
+		if (strncmp("name", &json[tokens[i].start], 4) != 0 &&
+			strncmp("location", &json[tokens[i].start], 8) != 0 &&
+			strncmp("offset", &json[tokens[i].start], 6) != 0 &&
+			strncmp("buttons", &json[tokens[i].start], 7) != 0)
+			return false;
+
+
+		if (strncmp("name", &json[tokens[i].start], 4) == 0)
+		{
+			if (name_found) //If this was already found, fail
+				return false;
+			name_found = true;
+
+			//This is the actual name of the entry
+			if (++i >= amount || tokens[i].type != JSMN_STRING)
+				return false;
+
+		}
+		else if (strncmp("location", &json[tokens[i].start], 8) == 0)
+		{
+			if (location_found)
+				return false;
+			location_found = true;
+
+			//Should be the actual location of the entry
+			if (++i >= amount || tokens[i].type != JSMN_STRING)
+				return false;
+
+		}
+		else if (strncmp("buttons", &json[tokens[i].start], 7) == 0)
+		{
+			if (buttons_found)
+				return false;
+			buttons_found = true;
+
+
+			//Should be an array of buttons
+			if (++i >= amount || tokens[i].type != JSMN_ARRAY)
+				return false;
+
+			size_t button_count = (size_t)tokens[i].size;
+
+			for (size_t k = 0; k < button_count; ++k)
+			{
+				//Should be a string from a pre-determined set
+				if (++i >= amount || tokens[i].type != JSMN_STRING)
+					return false;
+			}
+		}
+		else //due to the earlier if, this has to be offset
+		{
+			if (offset_found)
+				return false;
+			offset_found = true;
+
+			//Should be a number
+			if (++i >= amount || tokens[i].type != JSMN_PRIMITIVE)
+				return false;
+		}
+	}
+
+	*current_element = i;
+
+	return name_found && location_found && buttons_found;
+
+}
+
 static bool validate_json_tokens(const char* json, const jsmntok_t *tokens, size_t amount)
 {
 	size_t i = 0;
@@ -79,57 +168,8 @@ static bool validate_json_tokens(const char* json, const jsmntok_t *tokens, size
 
 	for (size_t j = 0; j < array_size; ++j)
 	{
-		//Should be the object wrapping an entry
-		if (++i >= amount || tokens[i].type != JSMN_OBJECT)
+		if (!validate_json_entry_token(json, tokens, amount, &i))
 			return false;
-
-		//Should be "name"
-		if (++i >= amount ||
-			tokens[i].type != JSMN_STRING ||
-			strncmp("name", &json[tokens[i].start], 4) != 0)
-			return false;
-
-		//This is the actual name of the entry
-		if (++i >= amount || tokens[i].type != JSMN_STRING)
-			return false;
-
-		//Should be "location"
-		if (++i >= amount ||
-			tokens[i].type != JSMN_STRING ||
-			strncmp("location", &json[tokens[i].start], 8) != 0)
-			return false;
-
-		//Should be the actual location of the entry
-		if (++i >= amount || tokens[i].type != JSMN_STRING)
-			return false;
-
-		//Should be "offset"
-		if (++i >= amount ||
-			tokens[i].type != JSMN_STRING ||
-			strncmp("offset", &json[tokens[i].start], 6) != 0)
-			return false;
-
-		//Should be a number
-		if (++i >= amount || tokens[i].type != JSMN_PRIMITIVE)
-			return false;
-
-		//Should be "buttons"
-		if (++i >= amount ||
-			tokens[i].type != JSMN_STRING ||
-			strncmp("buttons", &json[tokens[i].start], 7) != 0)
-			return false;
-
-		//Should be an array of buttons
-		if (++i >= amount || tokens[i].type != JSMN_ARRAY)
-			return false;
-
-		size_t button_count = (size_t)tokens[i].size;
-		for (size_t k = 0; k < button_count; ++k)
-		{
-			//Should be a string from a pre-determined set
-			if (++i >= amount || tokens[i].type != JSMN_STRING)
-				return false;
-		}
 	}
 
 	return true;
@@ -181,55 +221,81 @@ static bool token_extract_button(const char *json, const jsmntok_t *token, ctr_h
 	return false;
 }
 
+static bool parse_json_entry_token(const char* json, const jsmntok_t *tokens, size_t amount, size_t *current_element, a9l_config_entry *config_entry)
+{
+	size_t i = *current_element;
+
+	//Mandatory entries: name, location, buttons(for now)
+	//const char *name;
+	char *location = NULL;
+	ctr_hid_button_type buttons = CTR_HID_NONE;
+	size_t offset = 0;
+
+	size_t number_of_entries = (size_t)tokens[i].size;
+	i++;
+
+	for (size_t entry = 0; entry < number_of_entries; ++entry)
+	{
+		const char * key = &json[tokens[i++].start];
+		if (strncmp("name", key, 4) == 0)
+		{
+			//name = token_extract_string(json, &tokens[i]);
+		}
+		else if (strncmp("location", key, 8) == 0)
+		{
+			location = token_extract_string(json, &tokens[i]);
+		}
+		else if (strncmp("buttons", key, 7) == 0)
+		{
+			size_t button_count = (size_t)tokens[i++].size;
+			for (size_t k = 0; k < button_count; ++k)
+			{
+				//Should be a string from a pre-determined set
+				ctr_hid_button_type button;
+				if (!token_extract_button(json, &tokens[i++], &button))
+				{
+					free(location);
+					return false;
+				}
+				buttons |= button;
+			}
+			--i; //Let the end of the loop update i to point to the next entry
+		}
+		else //due to the earlier if, this has to be offset
+		{
+			char *end = NULL;
+			offset = (size_t)token_extract_long(json, &tokens[i], &end);
+			if (&json[tokens[i].end] != end)
+			{
+				free(location);
+				return false;
+			}
+		}
+		++i;
+	}
+
+	a9l_config_entry_initialize(config_entry, location, offset, buttons);
+	free(location);
+
+	*current_element = i;
+
+	return true;
+
+}
+
 static bool parse_json_tokens(const char* json, const jsmntok_t *tokens, size_t amount, a9l_config *config)
 {
 	size_t i = 2;
-	size_t array_size = (size_t)tokens[i].size;
+	size_t array_size = (size_t)tokens[i++].size;
 	a9l_config_initialize(config, array_size);
 
 	for (size_t j = 0; j < array_size; ++j)
 	{
-		//This is the actual name of the entry
-		i += 3;
-
-		//Should be the actual location of the entry
-		i += 2;
-		char *payload = token_extract_string(json, &tokens[i]);
-
-		//Should be a number
-		i += 2;
-		char *end = NULL;
-		size_t offset = (size_t)token_extract_long(json, &tokens[i], &end);
-
-		if (&json[tokens[i].end] != end)
+		a9l_config_entry *entry = a9l_config_get_entry(config, j);
+		if (!parse_json_entry_token(json, tokens, amount, &i, entry))
 		{
-			a9l_config_destroy(config);
-			free(payload);
 			return false;
 		}
-
-		//Should be an array of buttons
-		i += 2;
-
-		size_t button_count = (size_t)tokens[i].size;
-		ctr_hid_button_type buttons = 0;
-		for (size_t k = 0; k < button_count; ++k)
-		{
-			//Should be a string from a pre-determined set
-			++i;
-			ctr_hid_button_type button;
-			if (!token_extract_button(json, &tokens[i], &button))
-			{
-				a9l_config_destroy(config);
-				free(payload);
-				return false;
-			}
-			buttons |= button;
-		}
-
-		a9l_config_entry *entry = a9l_config_get_entry(config, j);
-		a9l_config_entry_initialize(entry, payload, offset, buttons);
-		free(payload);
 	}
 
 	return true;
